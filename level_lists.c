09765e43dfe4ae6b3ef1d59438679fb0945ea83f
Louis Hislaire <122865223+GitAzuXo@users.noreply.github.com>
2023-11-13 19:30:36 +0100

    Push for today

--- level_lists.c
+++ level_lists.c
@@ -3,3 +3,37 @@
 //
 
 #include "level_lists.h"
+
+t_d_list * emptyLevelList(int nbLevel){
+    t_d_list * list = (t_d_list *) malloc(sizeof(t_d_list));
+    t_d_cell** head = (t_d_cell **) malloc(sizeof(t_d_cell *) * nbLevel);
+
+    for(int i=0; i<nbLevel; i++){
+        head[i] = NULL;
+    }
+
+    list->heads = head;
+    return list;
+}
+
+void insertCellatHead(t_d_cell * cell, t_d_list * list){
+
+    // For all the levels in the cell insert at the head
+    for(int i=1; i<(list->height - 1); i++){
+        list->heads[i] = list->heads[i+1];
+    }
+    list->heads[0] = cell;
+
+}
+
+void displayListForLevel(t_d_list * list, int level){
+    int cell = 0;
+    t_d_cell *tmp;
+    tmp = list->heads[level];
+    while(tmp != NULL){
+        printf("Level : %d, Cell : %d, Value : %d", level, cell, tmp->value);
+        tmp = tmp->nexts[cell++];
+    }
+
+}
+
--- level_lists.h
+++ level_lists.h
@@ -6,10 +6,14 @@
 #define C_PROJECT_L2_LEVEL_LISTS_H
 
 #include "level_cells.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct s_d_list{
     t_d_cell** heads;
     int height;
 }t_d_list;
+
+t_d_list * emptyLevelList(int nbLevel);
 
 #endif //C_PROJECT_L2_LEVEL_LISTS_H
